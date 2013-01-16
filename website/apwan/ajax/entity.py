94c8dae78925160cc981d3812945dcd4e9511413
Dean Gardiner <gardiner91@gmail.com>
2013-01-17 02:11:33 +1300

    added example data loader to entity.search debug page (index.html), base laid for movie entity generation, added [keys.py.example] which needs to be setup per-user for development, modified [requirements.txt] to use zip releases for modified project forks.

--- website/apwan/ajax/entity.py
+++ website/apwan/ajax/entity.py
@@ -17,6 +17,8 @@ def search(request, type=None,
            artist=None, album=None, track=None):
     print "entity.search type:", type
 
+    entities = None
+
     try:
         if type == Entity.TYPE_MUSIC:
             if title is not None or artist is None:
@@ -25,8 +27,6 @@ def search(request, type=None,
             print "TYPE_MUSIC", '"' + str(artist) + '"', '"' + str(album) + '"', '"' + str(track) + '"'
 
             results = _entity_search(artist=artist, album=album, track=track)
-
-            entities = None
 
             if len(results) == 1:
                 entities = [results[0].dict(full=True)]
@@ -43,7 +43,25 @@ def search(request, type=None,
                 entities = [entity.dict(full=True)]
 
             return cors_response(simplejson.dumps({'success': True, 'items': entities}))
+        elif type == Entity.TYPE_MOVIE:
+            if artist is not None or album is not None or track is not None:
+                return cors_response(simplejson.dumps({'error': 'INVALID_PARAMETER', 'success': False}))
 
+            print "TYPE_MOVIE", '"' + str(title) + '"'
+
+            results = _entity_search(title=title)
+
+            if len(results) == 1:
+                entities = [results[0].dict(full=True)]
+            elif len(results) > 1:
+                entities = []
+                for entity in results:
+                    entities.append(entity.dict())
+            else:
+                # Lookup Details
+                print "looking up"
+
+            return cors_response(simplejson.dumps({'success': True, 'items': entities}))
         else:
             return cors_response(simplejson.dumps({'error': 'TYPE_NOT_IMPLEMENTED', 'success': False}))
 
@@ -54,34 +72,39 @@ def search(request, type=None,
 def _entity_search(**values):
     query = {}
 
-    if 'artist' in values:
+    if 'title' in values:
+        if values['title'] is not None:
+            query['s_title__ilike'] = search_like(values['title'])
+        else:
+            raise ValueError()
+    elif 'artist' in values:
         if values['artist'] is not None:
             query['s_artist__ilike'] = search_like(values['artist'])
         else:
             raise ValueError()  # not None artist required
-    else:
-        raise ValueError()
 
-    if 'album' in values:
-        values['album'] = search_like(values['album'])
+        if 'album' in values:
+            values['album'] = search_like(values['album'])
 
-        if values['album'] is not None and values['album'] != '':
-            query['s_album__ilike'] = values['album']
-        else:
-            query['album'] = None
+            if values['album'] is not None and values['album'] != '':
+                query['s_album__ilike'] = values['album']
+            else:
+                query['album'] = None
 
-    if 'track' in values:
-        values['track'] = search_like(values['track'])
+        if 'track' in values:
+            values['track'] = search_like(values['track'])
 
-        if values['track'] is not None and values['track'] != '':
-            query['s_track__ilike'] = values['track']
+            if values['track'] is not None and values['track'] != '':
+                query['s_track__ilike'] = values['track']
 
-            if 'album' in query and query['album'] is None:
-                query.pop('album')
+                if 'album' in query and query['album'] is None:
+                    query.pop('album')
+            else:
+                query['track'] = None
         else:
             query['track'] = None
     else:
-        query['track'] = None
+        raise ValueError()
 
     print query
 
---
+++ website/apwan/helpers/entitygen/movie.py
@@ -0,0 +1,11 @@
+import tmdb
+
+__author__ = 'Dean Gardiner'
+
+tmdb.configure()
+
+
+class MovieEntityGenerator():
+    @staticmethod
+    def lookup(title):
+        pass
