273ccf7ce0adb5c92216e7d478e5246ea994ecba
Dean Gardiner <gardiner91@gmail.com>
2013-01-18 04:52:17 +1300

    added payee/recipient/report page placeholders, few style changes to account sidebar.

--- website/apwan/views/account.py
+++ website/apwan/views/account.py
@@ -9,3 +9,18 @@ __author__ = 'Dean Gardiner'
 def index(request):
     return render_to_response('account/index.html',
                               context_instance=RequestContext(request, {}))
+
+
+def payee_add(request):
+    return render_to_response('account/payee/add.html',
+                              context_instance=RequestContext(request, {}))
+
+
+def recipient_claim(request):
+    return render_to_response('account/recipient/claim.html',
+                              context_instance=RequestContext(request, {}))
+
+
+def report_donations(request):
+    return render_to_response('account/report/donations.html',
+                              context_instance=RequestContext(request, {}))
--- website/urls.py
+++ website/urls.py
@@ -13,10 +13,11 @@ urlpatterns = patterns('',
 
     # Account
     url('^account/$', 'website.apwan.views.account.index'),
-    url('^account/login/$', 'django.contrib.auth.views.login', {
-        'template_name': 'account/login.html'
-    }),
+    url('^account/login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
     url('^account/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/home'}),
+    url('^account/payee/add/$', 'website.apwan.views.account.payee_add'),
+    url('^account/recipient/claim/$', 'website.apwan.views.account.recipient_claim'),
+    url('^account/report/donations/$', 'website.apwan.views.account.report_donations'),
 
     # Dev
     url('^dev/$', 'website.apwan.views.dev.index'),
