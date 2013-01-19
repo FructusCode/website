2d46923fc68a9839f8ec7f5910b4b5b17e74fa4e
Dean Gardiner <gardiner91@gmail.com>
2013-01-19 18:40:44 +1300

    Validation part of donation.checkout done.

--- website/apwan/__init__.py
+++ website/apwan/__init__.py
@@ -1,5 +1,3 @@
 USERAGENT_NAME = "Fruct.us"
 USERAGENT_VERSION = "0.1"
 USERAGENT_CONTACT = "team@fruct.us"
-
-import ajax
--- website/apwan/ajax/donation.py
+++ website/apwan/ajax/donation.py
@@ -1,10 +1,27 @@
 from dajaxice.decorators import dajaxice_register
 from django.utils import simplejson
+from website.apwan.ajax.utils import cors_response
+from website.apwan.models.entity import Entity
 
 __author__ = 'Dean Gardiner'
 
 
 @dajaxice_register(method='GET', name='donation.checkout')
 def checkout(request, recipient_id, entity_id, amount):
-    print recipient_id, entity_id, amount
-    return simplejson.dumps({})
+    # Get entity
+    entity = Entity.objects.filter(pk=entity_id)
+    if len(entity) != 1:
+        return cors_response(simplejson.dumps({'error': 'INVALID_PARAMETER', 'error_parameter': 'entity_id', 'success': False}))
+    entity = entity[0]
+
+    # Get recipient from entity (this will also confirm the entity and recipient are linked)
+    recipient = entity.recipient.filter(pk=recipient_id)
+    if len(recipient) != 1:
+        return cors_response(simplejson.dumps({'error': 'INVALID_PARAMETER', 'error_parameter': 'recipient_id', 'success': False}))
+    recipient = recipient[0]
+
+    payee = recipient.payee
+    if payee is None:
+        return cors_response(simplejson.dumps({'error': 'NO_PAYEE', 'success': False}))
+
+    print "payee account_id:", payee.account_id
