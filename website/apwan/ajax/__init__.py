bcee990da9baa10f74f8a17ff7f18f998500b30a
Dean Gardiner <gardiner91@gmail.com>
2013-02-09 21:08:32 +1300

    ignoring W0403 and C0111 warnings in apwan.ajax and apwan.models

--- website/apwan/ajax/__init__.py
+++ website/apwan/ajax/__init__.py
@@ -1,5 +1,10 @@
-__author__ = 'Dean Gardiner'
+# pylint: disable=W0403
+# pylint: disable=C0111
 
 import donation
+
 import entity
+
 import recipient
+
+__author__ = 'Dean Gardiner'
--- website/apwan/models/__init__.py
+++ website/apwan/models/__init__.py
@@ -1,10 +1,18 @@
+# pylint: disable=W0403
+# pylint: disable=C0111
+
 import donation
+
 import entity
 import entity_reference
+
 import payee
+
 import recipient
 import recipient_reference
+
 import service
+
 import user_profile
 
 __author__ = 'Dean Gardiner'
