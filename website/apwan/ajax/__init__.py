33788c42e2586ad7e13557472ea7d4042c14ab01
Dean Gardiner <gardiner91@gmail.com>
2013-02-09 21:12:49 +1300

    Fixed pyflakes issues in apwan.ajax and apwan.models

--- website/apwan/ajax/__init__.py
+++ website/apwan/ajax/__init__.py
@@ -2,9 +2,12 @@
 # pylint: disable=C0111
 
 import donation
+assert donation
 
 import entity
+assert entity
 
 import recipient
+assert recipient
 
 __author__ = 'Dean Gardiner'
--- website/apwan/models/__init__.py
+++ website/apwan/models/__init__.py
@@ -2,17 +2,27 @@
 # pylint: disable=C0111
 
 import donation
+assert donation
 
 import entity
+assert entity
+
 import entity_reference
+assert entity_reference
 
 import payee
+assert payee
 
 import recipient
+assert recipient
+
 import recipient_reference
+assert recipient_reference
 
 import service
+assert service
 
 import user_profile
+assert user_profile
 
 __author__ = 'Dean Gardiner'
