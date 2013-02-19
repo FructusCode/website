a815ff517c64e0eb1118c4b3f472b612b6a17883
Dean Gardiner <gardiner91@gmail.com>
2013-02-20 00:09:42 +1300

    Fix to [helper_tags.active] when "/account/payee/my-payee-2" was matching for both "/account/payee/my-payee" and "/account/payee/my-payee-2".

--- website/apwan/templatetags/helper_tags.py
+++ website/apwan/templatetags/helper_tags.py
@@ -11,6 +11,13 @@ def active(request, pattern):
     if not pattern.startswith('/'):
         pattern = reverse(pattern)
 
+    if pattern[-1] != '/' or pattern[-1] != '\\':
+        pattern += '/'
+
+    path = request.path
+    if path[-1] != '/' or path[-1] != '\\':
+        path += '/'
+
     if request.path.startswith(pattern):
         return 'active'
 
